570dea7f8a33c318992de9cebc0c1da5d8f5f6f5
samueledellavedova <46026355+samueledellavedova@users.noreply.github.com>
2022-04-28 14:39:11 +0200

    refactor(hex): group chunk decoding

--- src/hex.rs
+++ src/hex.rs
@@ -1,10 +1,14 @@
-fn decode_byte(byte: u8) -> u8 {
-  match byte {
-    b'0'..=b'9' => byte - b'0',
-    b'a'..=b'f' => byte - b'a' + 10,
-    b'A'..=b'F' => byte - b'A' + 10,
-    _ => panic!("hex: invalid character"),
+fn decode_chunk(chunk: &[u8]) -> u8 {
+  fn convert(byte: u8) -> u8 {
+    match byte {
+      b'0'..=b'9' => byte - b'0',
+      b'a'..=b'f' => byte - b'a' + 10,
+      b'A'..=b'F' => byte - b'A' + 10,
+      _ => panic!("hex: invalid character"),
+    }
   }
+
+  convert(chunk[0]) << 4 | convert(chunk[1])
 }
 
 pub fn decode<T: AsRef<[u8]>>(bytes: T) -> Vec<u8> {
@@ -16,6 +20,6 @@ pub fn decode<T: AsRef<[u8]>>(bytes: T) -> Vec<u8> {
 
   bytes
     .chunks(2)
-    .map(|chunk| decode_byte(chunk[0]) << 4 | decode_byte(chunk[1]))
+    .map(decode_chunk)
     .collect()
 }
