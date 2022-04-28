ae20b3e16f25638e82e6725e4b88d01030f5f4f1
samueledellavedova <46026355+samueledellavedova@users.noreply.github.com>
2022-04-28 12:03:52 +0200

    feat: add challenge 1

---
+++ src/challenges/c1.rs
@@ -0,0 +1,11 @@
+use cryptopals::hex;
+use cryptopals::base64;
+
+const INPUT: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+const OUTPUT: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+pub fn run() {
+  println!("CHALLENGE 1...");
+  assert_eq!(String::from_utf8_lossy(&base64::encode(&hex::decode(INPUT))), OUTPUT);
+  println!("OK.")
+}
